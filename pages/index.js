137e52c227ac9275426c2e94571b25a6128318df
Lee-Mather-Livv <lee.mather@livvhousinggroup.com>
2025-06-08 20:10:48 +0100

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
-// This will be replaced with full UI content
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Livv Grounds Maintenance Portal</h1>
+      <p>The portal has been set up correctly with three user roles.</p>
+      <p>This is a placeholder while your full system loads.</p>
+    </div>
+  );
+}
